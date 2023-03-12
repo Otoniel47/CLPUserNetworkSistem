801eb0a55029e294f41cf69476e3c20405c98b53
Otoniel Candido <37228777+Otoniel47@users.noreply.github.com>
2023-03-12 00:37:24 -0300

    Controller User

---
+++ CLPUserNetworkSistem/CLPUserNetworkSistem/Telas/TelaLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLPUserNetworkSistem.Telas
+{
+    public partial class TelaLogin : UserControl
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
